634bf9cd8b0937d1b59315d610d25141bf97c81b
Leopold-Klotz <leopoldk3@gmail.com>
2024-03-13 20:55:37 -0700

    speed up the rotation

--- sentry.py
+++ sentry.py
@@ -12,7 +12,7 @@ MISTAKE_THRESHOLD = 0.2
 HANDTRACKING_LIMIT = 45 # 45 seconds of hand control
 CONTROL_LANDMARKS = [8, 4]  # landmarks for the tip of the pointer and thumb (change to thumb?)
 COMMAND_HOLD_MS = 5
-ROTATION_STEP = 2
+ROTATION_STEP = 5
 
 class Sentry:
     def __init__(self, port):
